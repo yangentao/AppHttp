d4b13a8e7a8baac4258a7a1fa5f1a4f5cbd734f0
yangentao <entaoyang@163.com>
2022-08-27 16:27:03 +0800

    0

---
+++ app/src/main/java/dev/entao/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.entao.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
